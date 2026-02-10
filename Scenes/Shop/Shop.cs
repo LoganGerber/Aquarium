fdb20f66a2c1e1f13795af541a9d8005b8187d73
Logan Gerber <logangerber@protonmail.com>
2026-02-09 16:53:01 -0800

    Update shop for fish string update, add button events

--- Scenes/Shop/Shop.cs
+++ Scenes/Shop/Shop.cs
@@ -3,24 +3,61 @@ using System;
 
 public partial class Shop : PanelContainer
 {
+	[Signal]
+	public delegate void BuyButtonPressedEventHandler(string selectedFish);
+
+	[Signal]
+	public delegate void ExitButtonPressedEventHandler();
+
+
 	[Export]
 	private PackedScene fishSelectionScene;
 
 	private GridContainer optionsGrid;
 
+	private string currentFishSelection = null;
+
+	private Button buyButton;
+
+	private Button exitButton;
+
+
+
 	public override void _Ready()
 	{
 		base._Ready();
 		optionsGrid = GetNode<GridContainer>("%FishOptions");
+		buyButton = GetNode<Button>("%BuyButton");
+		exitButton = GetNode<Button>("%ExitButton");
 
-		FishManager.FISH_TYPE[] allFishTypes = Enum.GetValues<FishManager.FISH_TYPE>();
-		foreach (FishManager.FISH_TYPE fish in allFishTypes)
+		string[] allFishTypes = ConfigManager.Instance.GetAllFishTypes();
+		foreach (string fish in allFishTypes)
 		{
-			Button fishButton = fishSelectionScene.Instantiate<Button>();
-			fishButton.Text = FishManager.GetHumanReadableFishName(fish);
-			fishButton.Icon = FishManager.GetFishTexture(fish);
+			FishSelection fishButton = fishSelectionScene.Instantiate<FishSelection>();
+			fishButton.Text = fish;
+			fishButton.Icon = FishManager.Instance.GetFishTexture(fish);
+			fishButton.FishType = fish;
+			fishButton.FishSelected += OnFishSelected;
 
 			optionsGrid.AddChild(fishButton);
 		}
+
+		buyButton.Disabled = true;
+	}
+
+	public void OnFishSelected(string fishType)
+	{
+		currentFishSelection = fishType;
+		buyButton.Disabled = false;
+	}
+
+	public void OnBuyButtonPressed()
+	{
+		EmitSignal(SignalName.BuyButtonPressed, currentFishSelection);
+	}
+
+	public void OnExitButtonPressed()
+	{
+		EmitSignal(SignalName.ExitButtonPressed);
 	}
 }
