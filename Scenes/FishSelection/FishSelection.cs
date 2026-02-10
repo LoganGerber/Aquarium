aa860fc4a7ed8a4dfb88862c6adf6c8379df7008
Logan Gerber <logangerber@protonmail.com>
2026-02-09 16:52:15 -0800

    Add fish name to FishSelection pressed event

--- Scenes/FishSelection/FishSelection.cs
+++ Scenes/FishSelection/FishSelection.cs
@@ -1,34 +1,23 @@
 using Godot;
-using System;
 
-public partial class FishSelection : PanelContainer
+public partial class FishSelection : Button
 {
-	private readonly StyleBoxFlat unselected = GD.Load<StyleBoxFlat>("res://Scenes/FishSelection/Styles/Unselected.tres");
-	private readonly StyleBoxFlat selected = GD.Load<StyleBoxFlat>("res://Scenes/FishSelection/Styles/Selected.tres");
-	private readonly StyleBoxFlat hovered = GD.Load<StyleBoxFlat>("res://Scenes/FishSelection/Styles/Hovered.tres");
+	[Signal]
+	public delegate void FishSelectedEventHandler(string fishType);
+
+
+	public string FishType { get; set; }
 
 
 	public override void _Ready()
 	{
 		base._Ready();
-		Button button = GetNode<Button>("%Button");
-		button.Toggled += OnToggled;
-		button.MouseEntered += OnMouseEntered;
-		button.MouseExited += OnMouseExited;
+		Pressed += OnPressed;
 	}
 
-	private void OnToggled(bool toggledOn)
+
+	public void OnPressed()
 	{
-
-	}
-
-	private void OnMouseEntered()
-	{
-
-	}
-
-	private void OnMouseExited()
-	{
-
+		EmitSignal(SignalName.FishSelected, FishType);
 	}
 }
