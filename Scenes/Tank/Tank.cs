83d38a010e4d1fa8f75c531365dbac119540a53d
Logan Gerber <logangerber@protonmail.com>
2026-02-09 16:53:11 -0800

    Get shop working

--- Scenes/Tank/Tank.cs
+++ Scenes/Tank/Tank.cs
@@ -1,14 +1,44 @@
 using Godot;
-using System;
 
-public partial class Tank : Node2D
+public partial class Tank : Control
 {
 	private FishManager fishManager;
+	private Shop shop;
+	private TextureButton shopIcon;
+
 
 
 	public override void _Ready()
 	{
 		base._Ready();
 
+		shop = GetNode<Shop>("%Shop");
+		shopIcon = GetNode<TextureButton>("%ShopIcon");
+
+		DisableShop();
+		shop.ExitButtonPressed += OnShopExitButtonPressed;
+	}
+
+	public void OnShopIconPressed()
+	{
+		EnableShop();
+	}
+
+	public void OnShopExitButtonPressed()
+	{
+		DisableShop();
+	}
+
+
+	private void EnableShop()
+	{
+		shop.Visible = true;
+		shopIcon.Visible = false;
+	}
+
+	private void DisableShop()
+	{
+		shop.Visible = false;
+		shopIcon.Visible = true;
 	}
 }
