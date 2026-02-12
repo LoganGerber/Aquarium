791ae752f805b04cc03125144ff2df6602b23344
Logan Gerber <logangerber1014@yahoo.com>
2026-02-11 17:57:33 -0800

    Add debug input manager

---
+++ Scripts/DebugInputManager.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class DebugInputManager : Singleton<DebugInputManager>
+{
+	public override void _UnhandledKeyInput(InputEvent @event)
+	{
+		if (@event is InputEventKey keyEvent)
+		{
+			if (keyEvent.Pressed)
+			{
+				if (keyEvent.Keycode == Key.Minus)
+				{
+					MoneyManager.Instance.SpendMoney(20);
+					GD.Print($"DEBUG: Removing money. New total = {MoneyManager.Instance.GetCurrentMoney()}");
+					GetViewport().SetInputAsHandled();
+				}
+				else if (keyEvent.Keycode == Key.Equal)
+				{
+					MoneyManager.Instance.AddMoney(20);
+					GD.Print($"DEBUG: Adding money. New total = {MoneyManager.Instance.GetCurrentMoney()}");
+					GetViewport().SetInputAsHandled();
+				}
+			}
+		}
+	}
+}
