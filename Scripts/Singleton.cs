83067bafb5715bc97ab8dbe56875950c520ab084
Logan Gerber <logangerber@protonmail.com>
2026-02-22 18:49:46 -0800

    Change singleton to no use lazy

--- Scripts/Singleton.cs
+++ Scripts/Singleton.cs
@@ -1,21 +1,12 @@
 using Godot;
-using System;
 
 public abstract partial class Singleton<T> : Node where T : Singleton<T>
 {
-	private static Lazy<T> instance;
-
-	public static T Instance
-	{
-		get
-		{
-			return instance.Value;
-		}
-	}
+	public static T Instance { get; private set; }
 
 	public override void _Ready()
 	{
 		base._Ready();
-		instance = new Lazy<T>(() => GetNode<T>($"/root/{this.Name}"));
+		Instance = GetNode<T>($"/root/{this.Name}");
 	}
 }
